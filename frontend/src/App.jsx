cb5dc535debca89849c4f90aba4dd34a415043b9
Ninh <duyninh163@gmail.com>
2025-10-29 00:34:03 +0700

    add Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from "react-router";
 import { SignUpPage } from './pages/signup/SignUpPage'
 import { SignInPage } from './pages/signin/SignInPage';
+import Home from "./pages/home/Home";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="signin" element={<SignInPage />} />
+//       <Route path="signup" element={<SignUpPage />} />
+//     </Routes>
+//   );
+// }
 
 function App() {
-  return (
-    <Routes>
-      <Route path="signin" element={<SignInPage />} />
-      <Route path="signup" element={<SignUpPage />} />
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
