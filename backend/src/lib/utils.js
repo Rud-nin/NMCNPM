2d08d217bc9084b61a2e530f3034bea25bd3eea9
henrivu1285 <minecraftteam125@gmail.com>
2025-10-30 01:17:32 +0700

    update signup user, hash password

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d", //Hạn token là 7 ngày, chỉ tạo token với playload có userId
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development", // Gửi cookie chỉ qua HTTPS trong môi trường production
+  });
+
+  return token;
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { getConnection } from "./lib/db.js";
-
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
 const app = express();
 
 app.get('/', (req, res) => {
   return res.json("BACKEND");
 });
+
 
 app.get("/users", async (req, res) => {
   try {
@@ -21,3 +23,5 @@ app.get("/users", async (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log("server is running on port", process.env.PORT);
 });
+
+app.use("/api/auth", authRoutes);
