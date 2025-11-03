963afde1ab2345206baa971b6a75acf14acaf9a8
Giang <trinhhoanggiang05@gmail.com>
2025-11-03 09:59:47 +0700

    add toggleTheme Button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
-import { Routes, Route } from "react-router";
-import { SignUpPage } from "./pages/signup/SignUpPage";
-import { SignInPage } from "./pages/signin/SignInPage";
-import NotFoundPage from "./pages/notfound/NotFoundPage";
-import Home from "./pages/home/Home";
+import { Routes, Route } from 'react-router'
+import { SignUpPage } from './pages/signup/SignUpPage'
+import { SignInPage } from './pages/signin/SignInPage'
+import NotFoundPage from './pages/notfound/NotFoundPage'
+import Home from './pages/home/Home'
+
+import ThemeToggle from './components/themeToggle/ThemeToggle'
+
+import './App.css'
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="signin" element={<SignInPage />} />
-			<Route path="signup" element={<SignUpPage />} />
-			<Route path="*" element={<NotFoundPage />} />
-		</Routes>
-	);
+  return (
+    <div className="app-container">
+      <ThemeToggle />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
