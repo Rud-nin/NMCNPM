6a72bc368b44ae8b39299b997f397f6ac887ec3a
Ninh <duyninh163@gmail.com>
2025-11-05 22:34:39 +0700

    Add Admin Dashboard template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { SignUpPage } from './pages/signup/SignUpPage'
 import { SignInPage } from './pages/signin/SignInPage'
 import NotFoundPage from './pages/notfound/NotFoundPage'
 import Home from './pages/home/Home'
+import AdminDashboard from './pages/admin_dashboard/AdminDashboard'
 
 import ThemeToggle from './components/themeToggle/ThemeToggle'
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="signin" element={<SignInPage />} />
         <Route path="signup" element={<SignUpPage />} />
+        <Route path="admin" element={<AdminDashboard />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
