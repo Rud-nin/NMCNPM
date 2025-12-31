debd3748a66d63e7139380e725b848ab45f0e9c6
Chu Van Son <cvson1126034@gmail.com>
2025-12-31 12:45:53 +0700

    feat: add feedback functionality to UserNotification component and create useFeedbackStore

--- frontend/src/components/UserNotification/UserNotification.jsx
+++ frontend/src/components/UserNotification/UserNotification.jsx
@@ -1,17 +1,26 @@
 import { useNotificationStore } from '../../stores/useNotificationStore.js';
+import { useFeedbackStore } from '../../stores/useFeedbackStore.js';
 import Button from '../../components/Button/Button.jsx';
 import Table from '../Table/Table.jsx';
 import styles from './UserNotification.module.css';
+import Overlay from '../Overlay/Overlay.jsx';
 import { useState, useEffect } from 'react';
 
 function UserNotification() {
   const [notifications, setNotifications] = useState([]);
+  const [feedback, setFeedback] = useState(null);
   const [sort, setSort] = useState("new-to-old");
   const getUserNotifications = useNotificationStore((s) => s.getUserNotifications);
+  const sendFeedback = useFeedbackStore((s) => s.sendFeedback);
 
   async function fetchNoti() {
     const data = await getUserNotifications();
     if (data) setNotifications(data);
+  }
+
+  async function handleSendFeedback() {
+    await sendFeedback(feedback.title, feedback.content);
+    setFeedback(null);
   }
 
   useEffect(() => {
@@ -30,6 +39,9 @@ function UserNotification() {
       <header>
         <h2>Thông báo</h2>
         <div className={styles.btnContainer}>
+          <Button
+            onClick={() => setFeedback({ title: '', content: '' })}
+          >Tạo phản hồi</Button>
           <Button
             onClick={fetchNoti}
           >Làm mới</Button>
@@ -75,13 +87,41 @@ function UserNotification() {
           ))}
         </tbody>
       </Table>
+
+      {feedback && (
+        <Overlay>
+          <div className={styles.modal}>
+            <h2>Nhập phản hồi mới</h2>
+            <div className={styles.input}>
+              <span>Tiêu đề</span>
+              <input
+                type="text"
+                value={feedback.title}
+                placeholder="Nhập tiêu đề phản hồi"
+                onChange={(e) => setFeedback({ ...feedback, title: e.target.value })} />
+            </div>
+            <div className={styles.input}>
+              <span>Nội dung</span>
+              <textarea
+                type="text"
+                value={feedback.content}
+                placeholder="Nhập nội dung phản hồi"
+                onChange={(e) => setFeedback({ ...feedback, content: e.target.value })} />
+            </div>
+            <div className={styles.btnContainer}>
+              <Button onClick={handleSendFeedback}>Gửi</Button>
+              <Button onClick={() => setFeedback(null)}>Hủy</Button>
+            </div>
+          </div>
+        </Overlay>
+      )}
     </section>
   );
 }
 
 function formatDateTime(dateTimeString) {
-  const dateTime = new Date(dateTimeString);  
-  
+  const dateTime = new Date(dateTimeString);
+
   const hh = String(dateTime.getHours()).padStart(2, "0");
   const mm = String(dateTime.getMinutes()).padStart(2, "0");
   const ss = String(dateTime.getSeconds()).padStart(2, "0");
